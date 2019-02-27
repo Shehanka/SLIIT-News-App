dc8aa6601e7e103e184a84ec51e3f5be1f66b659
Shehanka <hcsperera@outlook.com>
2019-02-27 11:35:13 +0530

    services added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'news', loadChildren: './news/news.module#NewsPageModule' }
+  { path: 'news', loadChildren: './news/news.module#NewsPageModule' },
+  { path: 'news-detail', loadChildren: './news-detail/news-detail.module#NewsDetailPageModule' }
 ];
 @NgModule({
   imports: [
